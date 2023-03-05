80ef917fc472a71a71cc44ad102122e60dd2a3ad
kky7 <kykim708@gmail.com>
2023-03-05 12:06:35 +0900

    2_7:점수 계산

--- src/main/java/org/cote/inflearn/c2_array/p07.java
+++ src/main/java/org/cote/inflearn/c2_array/p07.java
@@ -1,4 +1,26 @@
 package org.cote.inflearn.c2_array;
+import java.util.*;
 
 public class p07 {
+    public static int solution(int n, int[] arr){
+        int answer = 0;
+        int score = 0;
+        for(int x: arr){
+            if(x == 1) {
+                score += 1;
+                answer += score;
+            }
+            else score = 0;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(solution(n, arr));
+    }
 }
