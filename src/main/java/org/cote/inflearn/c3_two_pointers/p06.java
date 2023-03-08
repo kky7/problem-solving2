cdb66b5bec9c3808918aeea50cef35b6514fb7c6
kky7 <kykim708@gmail.com>
2023-03-09 06:09:21 +0900

    3_06: 최대 길이 연속 부분수열

---
+++ src/main/java/org/cote/inflearn/c3_two_pointers/p06.java
@@ -0,0 +1,29 @@
+package org.cote.inflearn.c3_two_pointers;
+import java.util.*;
+public class p06 {
+    public static int solution(int n, int k, int[] arr){
+        int answer = Integer.MIN_VALUE;
+        int lt = 0;
+        int cnt = 0;
+        for(int rt = 0; rt < n; rt++){
+            if(arr[rt] == 0) cnt++;
+
+            while(cnt > k){
+                if(arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        System.out.println(solution(n,k,arr));
+    }
+}
